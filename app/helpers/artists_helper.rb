b65b285d85810ff817757a139240bbe4fef01e38
Matthew McCormack <mmccormack1182@gmail.com>
2019-04-10 12:54:00 -0400

    done

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,12 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def artist_select(song, artist)
+    if artist.nil?
+      collection_select(:song, :artist_id, Artist.all, :id, :name)
+    else
+      display_artist(song)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :artists do
+  resources :artists, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
     resources :songs, only: [:index, :show, :new, :edit]
   end
-  resources :songs
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
 end
